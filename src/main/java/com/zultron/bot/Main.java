233e79ac5289d78405a03124d7a375c1a52a1c68
Alex Duthie <alexduthiemain@outlook.com>
2021-11-10 22:10:45 +0000

    Added Dummy Help Command

--- src/main/java/com/zultron/bot/Main.java
+++ src/main/java/com/zultron/bot/Main.java
@@ -2,12 +2,17 @@ package com.zultron.bot;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.util.Arrays;
 
-public class Main {
+public class Main extends ListenerAdapter {
 
-    private static final String token = "ODk3MTYxODM1OTUyODY5NDU3.YWRpEg.3or-HNLoME2VFnSCTcrzgwJrTnM";
+    private static final String token = System.getenv("Zultron_Token");
 
     public static void main(String[] args) {
 
@@ -15,12 +20,26 @@ public class Main {
 
         zultron.setStatus(OnlineStatus.ONLINE);
 
+        zultron.addEventListeners(new Main());
+
         try {
             zultron.build();
         } catch (LoginException exception) {
             exception.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        System.out.println(Arrays.toString(message));
+
+        if(message[0].equals("z!")) {
+            if(message[1].equals("help")) {
+                event.getChannel().sendMessage("I'm not helping YOU BOI").queue();
+            }
+        }
     }
 
 }
