6ccee4be4620ab8d9939d18e0816b78b1bd12b6e
Alex Duthie <alexduthiemain@outlook.com>
2021-11-12 01:11:34 +0000

    Help Class Updated

--- src/main/java/com/zultron/bot/command/Help.java
+++ src/main/java/com/zultron/bot/command/Help.java
@@ -10,12 +10,16 @@ public class Help extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
+
         if(message[0].equals("z!help")) {
+
             EmbedBuilder embedBuilder = new EmbedBuilder();
+
             embedBuilder.setTitle("Zultron Help");
             embedBuilder.setColor(0xF24343);
             embedBuilder.setDescription("Welcome to the Zultron Help Screen");
-            embedBuilder.setFooter("Work In Progress");
+            embedBuilder.setFooter("Work In Progress", event.getMessage().getAuthor().getAvatarUrl());
+
             event.getChannel().sendMessage(embedBuilder.build()).queue();
         }
     }
--- src/main/java/com/zultron/bot/main/Main.java
+++ src/main/java/com/zultron/bot/main/Main.java
@@ -1,8 +1,11 @@
 package com.zultron.bot.main;
 
-import com.zultron.bot.command.PrivateMessage;
+import com.zultron.bot.command.Greetings;
+import com.zultron.bot.command.Help;
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
@@ -15,8 +18,11 @@ public class Main extends ListenerAdapter {
         JDABuilder zultron = JDABuilder.createDefault(token);
 
         zultron.setStatus(OnlineStatus.ONLINE);
+        zultron.setActivity(Activity.playing("z!help"));
 
-        zultron.addEventListeners(new PrivateMessage());
+        zultron.addEventListeners(new Help());
+        zultron.addEventListeners(new Greetings());
+        zultron.enableIntents(GatewayIntent.GUILD_MEMBERS);
 
         try {
             zultron.build();
